6092d10d289e3b0d74dadede21417a093c2255e4
denshou <zecaumr@naver.com>
2025-02-17 20:55:49 +0900

    input 컴포넌트 퍼블리싱

---
+++ src/components/Input.tsx
@@ -0,0 +1,53 @@
+import { twMerge } from "tailwind-merge";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  className?: string;
+  isValid?: boolean;
+  errorMessage?: string;
+}
+
+export default function Input({
+  label,
+  id,
+  className,
+  isValid,
+  errorMessage,
+  ...props
+}: InputProps) {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={id} className="body-r text-gray-80 ml-[5px] mb-[2px]">
+        {label}
+      </label>
+      <input
+        id={id}
+        className={twMerge(
+          "w-full h-[38px] rounded-lg input-shadow outline-0 px-3 caption-m placeholder:text-gray-50",
+          !isValid ? "border-[1.5px] border-primary-active" : "",
+          className //사용자 정의 스타일
+        )}
+        {...props} //추가 속성
+      />
+      {!isValid && (
+        <p className="text-functional-danger text-[9px]/[18px] ml-[5px] mt-[2px]">
+          {errorMessage}
+        </p>
+      )}
+    </div>
+  );
+}
+
+//사용 예시
+{
+  // <Input
+  //   type="text"
+  //   id="nickname"
+  //   label="닉네임"
+  //   placeholder="닉네임을 입력해 주세요"
+  //   value={nickname}
+  //   onChange={(e) => setNickname(e.target.value)}
+  //   isValid={false}
+  //   errorMessage="닉네임 중복"
+  // />
+}

d314d68e1ef7aee47eb578fe221ffb981e65b213
denshou <zecaumr@naver.com>
2025-02-17 20:58:38 +0900

    input css 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -17,20 +17,20 @@ export default function Input({
 }: InputProps) {
   return (
     <div className="flex flex-col">
-      <label htmlFor={id} className="body-r text-gray-80 ml-[5px] mb-[2px]">
+      <label htmlFor={id} className="body-r text-gray-80 ml-[5px]">
         {label}
       </label>
       <input
         id={id}
         className={twMerge(
-          "w-full h-[38px] rounded-lg input-shadow outline-0 px-3 caption-m placeholder:text-gray-50",
+          "w-full h-[38px] rounded-lg input-shadow outline-0 px-3 caption-m placeholder:text-gray-50 my-[2px]",
           !isValid ? "border-[1.5px] border-primary-active" : "",
           className //사용자 정의 스타일
         )}
         {...props} //추가 속성
       />
       {!isValid && (
-        <p className="text-functional-danger text-[9px]/[18px] ml-[5px] mt-[2px]">
+        <p className="text-functional-danger text-[9px]/[18px] ml-[5px]">
           {errorMessage}
         </p>
       )}

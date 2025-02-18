b730ebc8e762bab36a771b60545e89b185a6db75
denshou <zecaumr@naver.com>
2025-02-18 17:15:26 +0900

    design:modal 컴포넌트 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import LandingLayout from '@/layouts/LandingLayout';
 import Landing from '@/pages/landing/Landing';
 import { Route, Routes } from 'react-router';
+import Modal from './components/Modal';
 
 function App() {
   return (
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,8 +1,9 @@
-import { useModalStore } from "@/store/modalStore";
+import { useModalStore } from '@/store/modalStore';
+import Button from './Button';
 
-interface Props {}
+interface ModalProps {}
 
-export default function Modal({}: Props) {
+export default function Modal({}: ModalProps) {
   const { modal, closeModal } = useModalStore();
   if (!modal.isOpen) return null;
   return (
@@ -11,28 +12,43 @@ export default function Modal({}: Props) {
       onClick={closeModal}
     >
       <div
-        className="bg-white p-5 mx-5 rounded-lg card-shadow w-[660px] min-h-[148px] flex flex-col justify-between"
+        className="bg-white p-5 mx-5 rounded-lg card-shadow w-[287px] min-h-[148px] flex flex-col justify-between"
         onClick={(e) => e.stopPropagation()}
       >
         <h2 className="h4-b text-center">{modal.title}</h2>
         <p className="caption-r text-center">{modal.message}</p>
         <div className="mt-4 flex justify-end gap-[6px]">
-          <button
-            className="bg-primary-normal text-white w-full min-w-[120px] h-[38px] rounded-lg"
-            onClick={modal.onConfirm || closeModal}
-          >
+          <Button type="primary" onClick={modal.onConfirm}>
             확인
-          </button>
+          </Button>
           {modal.onCancel && (
-            <button
-              className="bg-gray-300 w-full min-w-[120px] h-[38px] rounded-lg"
-              onClick={modal.onCancel}
-            >
+            <Button type="secondary" onClick={modal.onCancel}>
               취소
-            </button>
+            </Button>
           )}
         </div>
       </div>
     </div>
   );
+}
+// 사용 예시
+{
+  /* <button
+onClick={() =>
+  openModal({
+    title: '2버튼모달',
+    message: '입니다',
+    onConfirm() {
+      console.log('확인');
+      closeModal();
+    },
+    onCancel() {
+      console.log('취소');
+      closeModal();
+    },
+  })
+}
+>
+2
+</button> */
 }

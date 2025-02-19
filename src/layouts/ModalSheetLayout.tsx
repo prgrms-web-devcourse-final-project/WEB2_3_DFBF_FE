2a7119ca898ae41b9ba03c401966d31783310adb
kangsuyeong <ekdh16@naver.com>
2025-02-20 07:45:19 +0900

    feat: x버튼 sticky로 고정

--- src/layouts/ModalSheetLayout.tsx
+++ src/layouts/ModalSheetLayout.tsx
@@ -12,10 +12,10 @@ function ModalSheetLayout({ children, isOwnPost }: ModalSheetLayoutProps) {
     console.log('임시 함수');
   };
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="max-w-[600px] w-full h-screen flex flex-col bg-white rounded-[8px] card-shadow">
+    <div className="fixed inset-0 flex items-center justify-center z-50 pb-4">
+      <div className="max-w-[600px] w-full h-screen flex flex-col bg-white rounded-[8px] card-shadow overflow-y-auto">
         {/* 헤더 */}
-        <div className="flex h-[60px] items-center px-4 justify-between">
+        <div className="sticky top-0 flex min-h-[60px] h-[60px] items-center px-4 justify-between bg-white ">
           <button className="w-6 h-6 flex justify-center items-center cursor-pointer">
             <img src={closeIcon} alt="닫기" />
           </button>

2967651e7ecc3ef82f88397e3d6a48e3fedcf857
kangsuyeong <ekdh16@naver.com>
2025-02-19 11:52:31 +0900

    rename: 헤더 위치 변경

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,11 +1,13 @@
-import Header from '@/layouts/Header';
+import Header from '@/layouts/header/Header';
 import BottomNav from '@/layouts/BottomNav';
 import { Outlet } from 'react-router';
+import HeaderWithBack from '@/layouts/header/HeaderWithBack';
+import HeaderChat from '@/layouts/header/HeaderChat';
 
 function Layout() {
   return (
     <div className="max-w-[600px] w-full h-screen mx-auto flex flex-col">
-      <Header showMoreOptions />
+      <Header />
       <div className="flex-1 ">
         <Outlet />
       </div>

37ea5e174cde80369d7afa53bc20fd5dfad350aa
denshou <zecaumr@naver.com>
2025-02-19 21:15:26 +0900

    design: 지난 대화 기록 퍼블리싱

--- src/layouts/header/HedaerLayout.tsx
+++ src/layouts/header/HedaerLayout.tsx
@@ -1,6 +1,6 @@
 function HedaerLayout({ children }: { children: React.ReactNode }) {
   return (
-    <header className="fixed max-w-[600px] min-w-[320px] w-full h-[44px] bg-secondary-1/80 backdrop-blur-sm px-3 flex items-center">
+    <header className="fixed max-w-[600px] min-w-[320px] w-full h-[44px] bg-secondary-1/80 backdrop-blur-sm px-3 flex items-center z-40">
       {children}
     </header>
   );
--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,3 +1,46 @@
-export default function Chat() {
-  return <div>Chat</div>;
+import ChatHistoryCard from '@/components/ChatHistoryCard';
+
+export default function Chat({}) {
+  const mockItems = [
+    {
+      record_id: 101,
+      user_id: 1,
+      emotion: {
+        emotion_id: 1,
+        name: 'Happy',
+      },
+      spotify_music: {
+        spotify_id: 45,
+        title: '노래제목',
+        artist: '아티스트 이름',
+        album_image:
+          'https://i.namu.wiki/i/0FA-5SmcHSSGo7YD0RQvIBZKoU5d7BOKRVwoA4_RAeOx3HaNJT10ooyh55o4pC99vl2t5O5zzhILwwUougwJA9k8d6LHKGKAn3FylKTC4CIc378HalGwUBQG-phZjWU7lvjJ9LzkY3HEC0Bp72_I2Q.webp',
+      },
+      comment: '행복하당!',
+      created_at: '2025-02-19T11:00:00Z',
+    },
+    {
+      record_id: 102,
+      user_id: 2,
+      emotion: {
+        emotion_id: 3,
+        name: 'Sad',
+      },
+      spotify_music: {
+        spotify_id: 32,
+        title: '노래제목',
+        artist: '아티스트 이름',
+        album_image: null,
+      },
+      comment: '슬프다ㅠㅠ',
+      created_at: '2025-01-31T14:00:00Z',
+    },
+  ];
+  return (
+    <div className="w-full mt-[12px] mb-[58px] flex flex-col gap-4">
+      {mockItems.map((item) => (
+        <ChatHistoryCard item={item} key={item.record_id} />
+      ))}
+    </div>
+  );
 }

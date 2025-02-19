22ac9abf5653361670058ad61cff304dedef8520
yoonji <kyjkbw@gmail.com>
2025-02-19 14:44:03 +0900

    design: 레이아웃 및 헤더 색상 수정

--- src/layouts/header/HedaerLayout.tsx
+++ src/layouts/header/HedaerLayout.tsx
@@ -1,6 +1,6 @@
 function HedaerLayout({ children }: { children: React.ReactNode }) {
   return (
-    <header className="fixed max-w-[600px] w-full h-[44px] bg-[#FBF5FF]/90 backdrop-blur-sm px-3 flex items-center">
+    <header className="fixed max-w-[600px] w-full h-[44px] bg-secondary-1/80 backdrop-blur-sm px-3 flex items-center">
       {children}
     </header>
   );

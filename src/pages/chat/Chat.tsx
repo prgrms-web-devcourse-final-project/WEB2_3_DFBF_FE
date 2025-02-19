ee492f8cce3c79f277f8eae7711dbea2204fa537
정현승 <132555762+denshou@users.noreply.github.com>
2025-02-19 23:14:32 +0900

    fix: 지난 채팅 기록 mockdata 수정

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,45 +1,33 @@
 import ChatHistoryCard from '@/components/ChatHistoryCard';
 
 export default function Chat({}) {
-  const mockItems = [
+  const mockData = [
     {
-      record_id: 101,
-      user_id: 1,
-      emotion: {
-        emotion_id: 1,
-        name: 'Happy',
-      },
-      spotify_music: {
-        spotify_id: 45,
-        title: '노래제목',
-        artist: '아티스트 이름',
-        album_image:
-          'https://i.namu.wiki/i/0FA-5SmcHSSGo7YD0RQvIBZKoU5d7BOKRVwoA4_RAeOx3HaNJT10ooyh55o4pC99vl2t5O5zzhILwwUougwJA9k8d6LHKGKAn3FylKTC4CIc378HalGwUBQG-phZjWU7lvjJ9LzkY3HEC0Bp72_I2Q.webp',
-      },
-      comment: '행복하당!',
-      created_at: '2025-02-19T11:00:00Z',
+      chatroom_id: 3,
+      nickname: '길동이',
+      emotion: '행복한',
+      spotify_id: 33,
+      title: '라일락',
+      artist: '아이유',
+      album_image:
+        'https://i.namu.wiki/i/L4gbrOjwTsNcvpsCq8b4P-3eX9Cs0lrIvwHxtFE7S5jaeMsbdelvBqCLMwe6AJJw2zBQqSI4wE0_Qn-EwaeZdnLvseFvt1w9dg-xo9KrFF_GacO_R7BnHI6XRyDDXvr-PHMmSEnqgcrzLjdbQF9obA.webp',
+      created_at: '2025-02-19',
     },
     {
-      record_id: 102,
-      user_id: 2,
-      emotion: {
-        emotion_id: 3,
-        name: 'Sad',
-      },
-      spotify_music: {
-        spotify_id: 32,
-        title: '노래제목',
-        artist: '아티스트 이름',
-        album_image: null,
-      },
-      comment: '슬프다ㅠㅠ',
-      created_at: '2025-01-31T14:00:00Z',
+      chatroom_id: 1,
+      nickname: '철수',
+      emotion: '우울한',
+      spotify_id: 88,
+      title: 'Blue Sky',
+      artist: 'Cool Band',
+      album_image: null,
+      created_at: '2025-02-10',
     },
   ];
   return (
     <div className="w-full mt-[12px] mb-[58px] flex flex-col gap-4">
-      {mockItems.map((item) => (
-        <ChatHistoryCard item={item} key={item.record_id} />
+      {mockData.map((item) => (
+        <ChatHistoryCard item={item} key={item.chatroom_id} />
       ))}
     </div>
   );

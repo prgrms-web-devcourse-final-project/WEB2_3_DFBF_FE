57cfb711069eb55206dfa527d31d664f5d2f37a8
yoonji <kyjkbw@gmail.com>
2025-02-20 16:43:15 +0900

    feat: 감정 필터 컴포넌트 사용

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,15 @@
-import EmotionBadge from '@/components/EmotionBadge';
-import { EMOTIONS } from '@/constants';
 import SearchBar from '@/components/SearchBar';
-import { twMerge } from 'tailwind-merge';
 import MainCard from '@/components/MainCard';
+import { useState } from 'react';
+import EmotionFilter from '@/components/EmotionFilter';
 
 function Home() {
+  const [selectedEmotion, setSelectedEmotion] = useState<string | null>(null); // 선택된 감정
+  const handleClickedEmotion = (emotion: string) => {
+    setSelectedEmotion((prev) => (prev === emotion ? null : emotion));
+    console.log(emotion);
+  };
+
   return (
     <div className="flex flex-col gap-5 mt-5 h-fit w-full border border-amber-300">
       <SearchBar />
@@ -13,11 +18,7 @@ function Home() {
         <h2 className="font-saeeum text-2xl text-gray-60">
           나와 같은 감정을 느끼는 사람을 찾아보세요
         </h2>
-        <div className="w-fit grid grid-cols-4 gap-y-3 gap-x-5">
-          {EMOTIONS.map((emotion) => (
-            <EmotionBadge size="large" emotion={emotion.key} />
-          ))}
-        </div>
+        <EmotionFilter onEmotionClick={handleClickedEmotion} selectedEmotion={selectedEmotion} />
       </div>
       {/* 메인카드 리스트 */}
       <div className="flex flex-col items-center gap-2.5 pb-5">

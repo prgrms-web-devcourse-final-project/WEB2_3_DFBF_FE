0c3b7147fb6a097eb8e179b4e4d4171faf203537
yoonji <kyjkbw@gmail.com>
2025-02-21 07:47:33 +0900

    feat: 로그인 페이지에서 InputField 컴포넌트를 Input으로 변경

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,8 @@
 import Button from '@/components/Button';
-import InputField from '@/components/InputField';
 import logo from '@assets/icons/logo.svg';
 import kakao from '@assets/icons/kakao-icon.svg';
 import { Link } from 'react-router';
+import Input from '@/components/Input';
 export default function Login() {
   const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -20,20 +20,25 @@ export default function Login() {
         className="w-full pb-2.5 border-b border-gray-15"
       >
         <div className="flex flex-col gap-2 w-full">
-          <InputField type="text" id="userId" label="아이디" placeholder="아이디를 입력해 주세요" />
-          <InputField
-            type="password"
-            id="userPassword"
-            label="비밀번호"
-            placeholder="비밀번호를 입력해 주세요"
-          />
+          <div>
+            <label htmlFor="userId" className="body-r text-gray-80 ml-[5px] mb-0.5">
+              아이디
+            </label>
+            <Input id="userId" placeholder="아이디를 입력해 주세요" />
+          </div>
+          <div>
+            <label htmlFor="password" className="body-r text-gray-80 ml-[5px] mb-0.5">
+              비밀번호
+            </label>
+            <Input id="password" placeholder="비밀번호를 입력해 주세요" />
+          </div>
         </div>
         <Button className="focus:outline-primary-active mt-[14px]">로그인</Button>
       </form>
 
       <div className="flex flex-col gap-3 items-center w-full mt-2.5 ">
         {/* 소셜 로그인 */}
-        <Button className="bg-[#FFEB3B] text-gray-80 ">
+        <Button className="bg-[#FFEB3B] text-gray-80">
           <img src={kakao} alt="카카오 아이콘" />
           카카오 로그인
         </Button>

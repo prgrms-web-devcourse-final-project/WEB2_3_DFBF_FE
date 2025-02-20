38a7b424d85fefd81dab472897b947b876cb4123
yoonji <kyjkbw@gmail.com>
2025-02-20 16:42:03 +0900

    feat: 감정 필터 컴포넌트 추가

---
+++ src/components/EmotionFilter.tsx
@@ -0,0 +1,28 @@
+import EmotionBadge from '@/components/EmotionBadge';
+import { EMOTIONS } from '@/constants';
+import { twMerge } from 'tailwind-merge';
+
+interface EmotionFilterProps {
+  selectedEmotion: string | null;
+  onEmotionClick: (emotion: string) => void;
+}
+
+export default function EmotionFilter({ selectedEmotion, onEmotionClick }: EmotionFilterProps) {
+  return (
+    <div className="w-fit grid grid-cols-4 gap-y-3 gap-x-5">
+      {EMOTIONS.map((emotion) => (
+        <EmotionBadge
+          key={emotion.key}
+          size="large"
+          emotion={emotion.key}
+          isClickable={true}
+          onClick={onEmotionClick}
+          className={twMerge(
+            'transition-opacity duration-100 hover:opacity-70',
+            selectedEmotion && selectedEmotion !== emotion.key ? 'opacity-40' : 'opacity-100',
+          )}
+        />
+      ))}
+    </div>
+  );
+}

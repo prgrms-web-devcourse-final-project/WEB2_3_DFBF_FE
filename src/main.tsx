4eed356dd797fce63bb413f7bc034ba6d9b14b25
denshou <zecaumr@naver.com>
2025-02-20 14:36:28 +0900

    chore: tanstack-query main에 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { registerSW } from "virtual:pwa-register";
 import "./styles/index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
 
 // PWA 서비스 워커 등록
-import { registerSW } from "virtual:pwa-register";
-
 const updateSW = registerSW({
   onNeedRefresh() {
     if (confirm("새로운 버전이 있습니다. 새로고침할까요?")) {
@@ -17,8 +17,13 @@ const updateSW = registerSW({
   }
 });
 
+// 🔹 QueryClient 생성
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );

2b64902c37b26fa450c2035863daecbeaee4ffd5
denshou <zecaumr@naver.com>
2025-02-21 11:49:10 +0900

    feat: 음악 검색 기능 구현

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,9 +4,15 @@ import searchIconHover from '@assets/icons/search-icon/search-icon-hover.svg';
 import { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
+interface SearchBarProps {
+  isSticky?: boolean;
+  query: string;
+  setQuery: (query: string) => void;
+}
+
 // 검색바
 // isSticky를 props로 줄때 검색바 고정
-function SearchBar({ isSticky = false }: { isSticky?: boolean }) {
+function SearchBar({ isSticky = false, query, setQuery }: SearchBarProps) {
   const [icon, setIcon] = useState(searchIconDefault); // 아이콘
   return (
     <div
@@ -19,6 +25,8 @@ function SearchBar({ isSticky = false }: { isSticky?: boolean }) {
         type="text"
         className="w-full outline-none body-m placeholder:text-gray-400"
         placeholder="오늘의 음악을 검색해 보세요"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
       <button
         className="cursor-pointer"
--- src/components/modalSheet/MusicSearchSheet.tsx
+++ src/components/modalSheet/MusicSearchSheet.tsx
@@ -1,12 +1,77 @@
 import InfoMessage from '@/components/InfoMessage';
 import MusicSearchList from '@/components/modalSheet/MusicSearchList';
 import SearchBar from '@/components/SearchBar';
+import { useSpotifyAuth } from '@/hooks/useSpotifyAuth';
 import ModalSheetLayout from '@/layouts/ModalSheetLayout';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import defaultImage from '@assets/images/default.png';
+
+interface SpotifyMusic {
+  album: {
+    album_type: string;
+    artists: SpotifyArtist[];
+    available_markets: string[];
+    external_urls: { spotify: string };
+    href: string;
+    id: string;
+    images?: { url: string; height: number; width: number }[];
+    name: string;
+    release_date?: string;
+    release_date_precision?: string;
+    total_tracks?: number;
+    type: string;
+    uri: string;
+  };
+  artists: SpotifyArtist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: { isrc: string };
+  external_urls: { spotify: string };
+  href: string;
+  id: string;
+  is_local: boolean;
+  is_playable: boolean;
+  name: string;
+  popularity: number;
+  preview_url: string | null;
+  track_number: number;
+  type: string;
+  uri: string;
+}
+
+interface SpotifyArtist {
+  external_urls: { spotify: string };
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+}
 
 function MusicSearchSheet() {
   const [query, setQuery] = useState(''); // 검색어 상태
-  const [musics, setMusics] = useState([]); // 음악 목록을 저장
+  const [musics, setMusics] = useState<SpotifyMusic[]>([]); // 음악 목록을 저장
+
+  const { token } = useSpotifyAuth();
+
+  // spotify 음악 검색
+  const searchMusic = async (query: string) => {
+    const res = await fetch(
+      `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=track`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      },
+    );
+    const data = await res.json();
+    if (data.tracks.items.length > 0) {
+      console.log(data.tracks.items);
+      setMusics(data.tracks.items); // 검색된 트랙들 저장
+    } else {
+      setMusics([]); // 검색 결과가 없으면 빈 배열
+    }
+  };
 
   // 음악 리스트 그리는 함수
   const musicListRender = () => {
@@ -26,19 +91,31 @@ function MusicSearchSheet() {
         {musics.map((music, index) => (
           <MusicSearchList
             key={index}
-            albumImage="https://pbs.twimg.com/media/E68WkI4VIAIW8eT.jpg"
-            songTitle="Hype Boy"
-            artistName="NewJeans"
+            albumImage={music.album.images?.[0]?.url || defaultImage}
+            songTitle={music.name}
+            artistName={music.artists[0].name}
           />
         ))}
       </div>
     );
   };
 
+  useEffect(() => {
+    if (query === '') {
+      setMusics([]);
+      return;
+    }
+    const debounce = setTimeout(() => {
+      if (query.trim()) searchMusic(query);
+    }, 300); // 디바운스
+
+    return () => clearTimeout(debounce);
+  }, [query]);
+
   return (
     <ModalSheetLayout>
       <div className="flex flex-col h-full gap-4 px-3">
-        <SearchBar isSticky />
+        <SearchBar isSticky query={query} setQuery={setQuery} />
         {musicListRender()}
       </div>
     </ModalSheetLayout>

5dca8436728f37d328feda420edcd2027a6ece7b
yoonji <kyjkbw@gmail.com>
2025-02-24 14:25:32 +0900

    feat: Vite 설정에서 환경 변수 로드 기능 추가 및 API 프록시 URL 동적 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import tailwindcss from '@tailwindcss/vite';
@@ -6,93 +6,96 @@ import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate', // 서비스 워커 자동 업데이트
-      devOptions: {
-        enabled: true, // 개발 환경에서도 PWA 활성화
-      },
-      manifest: {
-        name: 'SoundLink',
-        short_name: 'SoundLink',
-        description: 'SoundLink',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: './icons/apple-touch-icon-57x57.png',
-            sizes: '57x57',
-            type: 'image/png',
-          },
-          {
-            src: './icons/apple-touch-icon-60x60.png',
-            sizes: '60x60',
-            type: 'image/png',
-          },
-          {
-            src: './icons/apple-touch-icon-72x72.png',
-            sizes: '72x72',
-            type: 'image/png',
-          },
-          {
-            src: './icons/apple-touch-icon-76x76.png',
-            sizes: '76x76',
-            type: 'image/png',
-          },
-          {
-            src: './icons/apple-touch-icon-114x114.png',
-            sizes: '114x114',
-            type: 'image/png',
-          },
-          {
-            src: './icons/apple-touch-icon-120x120.png',
-            sizes: '120x120',
-            type: 'image/png',
-          },
-          {
-            src: './icons/apple-touch-icon-144x144.png',
-            sizes: '144x144',
-            type: 'image/png',
-          },
-          {
-            src: './icons/apple-touch-icon-152x152.png',
-            sizes: '152x152',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://43.203.98.65:8080',
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        changeOrigin: true, // 백엔드 서버 주소로 변경
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        registerType: 'autoUpdate', // 서비스 워커 자동 업데이트
+        devOptions: {
+          enabled: true, // 개발 환경에서도 PWA 활성화
+        },
+        manifest: {
+          name: 'SoundLink',
+          short_name: 'SoundLink',
+          description: 'SoundLink',
+          start_url: '/',
+          display: 'standalone',
+          background_color: '#ffffff',
+          theme_color: '#000000',
+          icons: [
+            {
+              src: './icons/apple-touch-icon-57x57.png',
+              sizes: '57x57',
+              type: 'image/png',
+            },
+            {
+              src: './icons/apple-touch-icon-60x60.png',
+              sizes: '60x60',
+              type: 'image/png',
+            },
+            {
+              src: './icons/apple-touch-icon-72x72.png',
+              sizes: '72x72',
+              type: 'image/png',
+            },
+            {
+              src: './icons/apple-touch-icon-76x76.png',
+              sizes: '76x76',
+              type: 'image/png',
+            },
+            {
+              src: './icons/apple-touch-icon-114x114.png',
+              sizes: '114x114',
+              type: 'image/png',
+            },
+            {
+              src: './icons/apple-touch-icon-120x120.png',
+              sizes: '120x120',
+              type: 'image/png',
+            },
+            {
+              src: './icons/apple-touch-icon-144x144.png',
+              sizes: '144x144',
+              type: 'image/png',
+            },
+            {
+              src: './icons/apple-touch-icon-152x152.png',
+              sizes: '152x152',
+              type: 'image/png',
+            },
+          ],
+        },
+      }),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true, // 백엔드 서버 주소로 변경
+        },
       },
     },
-  },
-  // path설정
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@apis': fileURLToPath(new URL('./src/apis', import.meta.url)),
-      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@constants': fileURLToPath(new URL('./src/constants', import.meta.url)),
-      '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
-      '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
-      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
-      '@routes': fileURLToPath(new URL('./src/routes', import.meta.url)),
-      '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
-      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
-      '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
-      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+    // path설정
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@apis': fileURLToPath(new URL('./src/apis', import.meta.url)),
+        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@constants': fileURLToPath(new URL('./src/constants', import.meta.url)),
+        '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+        '@routes': fileURLToPath(new URL('./src/routes', import.meta.url)),
+        '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
+        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+        '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+        '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      },
     },
-  },
+  };
 });

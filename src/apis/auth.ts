ee49426a8d37c0158ff91a397a2e5a143101eaa7
yoonji <kyjkbw@gmail.com>
2025-02-25 11:16:19 +0900

    feat:  zustand persist 이용 전역 저장, 토큰 재발급 함수 추가

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -8,7 +8,8 @@ export const login = async (loginId: string, password: string) => {
       loginId,
       password,
     });
-    useAuthStore.getState().login(data.data.accessToken); // 토큰 전역 상태 저장
+    const accessToken = data.data.accessToken;
+    useAuthStore.getState().setAccessToken(accessToken); // 토큰 전역 상태 저장
     // console.log('로그인 됨', data);
     return data;
   } catch (error) {
@@ -25,6 +26,33 @@ export const logout = async () => {
     window.location.href = '/'; // 랜딩 페이지로 이동
   } catch (error) {
     console.log('로그아웃 에러', error);
+    throw error;
+  }
+};
+
+// 토큰 재발급
+export const reissueToken = async () => {
+  try {
+    const { data } = await axiosInstance.post('/auth/token');
+    const accessToken = data.data.accessToken; // 새로운 AT
+    const code = data.code; // 200: 성공, 400: 실패
+    console.log('토큰 재발급:', data);
+
+    if (code === 200) {
+      useAuthStore.getState().setAccessToken(accessToken); // 토큰 전역 상태 저장
+      console.log('AT 재발급 성공:', accessToken);
+    }
+
+    if (code === 400) {
+      useAuthStore.getState().logout(); // 전역 상태 초기화
+      console.log('토큰 재발급 실패:', data);
+      // window.location.href = '/'; // 랜딩 페이지로 이동
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    // window.location.href = '/'; // 랜딩  페이지로 이동
     throw error;
   }
 };

4d6d748436bd59292d6f20f4f53da40ee79a8c3e
kangsuyeong <ekdh16@naver.com>
2025-02-26 07:33:09 +0900

    feat: 이메일 인증을 하는 함수

--- src/apis/email.ts
+++ src/apis/email.ts
@@ -13,3 +13,11 @@ export const postEmailVerificationRequest = async (email: string) => {
   const response = await axiosInstance.post('/mail/verify', null, { params: { email } });
   return response;
 };
+
+// 이메일 인증을 확인하는 함수
+export const postEmailVerificationCheck = async (email: string, code: string) => {
+  const response = await axiosInstance.post('/mail/verify/check', null, {
+    params: { email, authCode: code },
+  });
+  return response;
+};

f5b79380e83fcfcd9d4221c21dad6e9da6e00221
kangsuyeong <ekdh16@naver.com>
2025-02-26 07:41:06 +0900

    feat: 인증번호 카운트

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-function CountdownTimer() {
+interface CountdownTimerProps {
+  onTimeout?: () => void; // 시간이 만료되었을 때 핸들링할 함수
+}
+
+function CountdownTimer({ onTimeout }: CountdownTimerProps) {
   const [timeLeft, setTimeLeft] = useState(300); // 300초 = 5분
 
   useEffect(() => {
-    if (timeLeft <= 0) return; // 타이머 종료
+    // 타이머 종료
+    if (timeLeft <= 0) {
+      if (onTimeout) onTimeout();
+      return;
+    }
     const timerId = setInterval(() => {
       setTimeLeft((prev) => prev - 1);
     }, 1000);

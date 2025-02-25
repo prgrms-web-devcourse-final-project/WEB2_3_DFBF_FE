aa9a4bf0966a53a205c8553d6673902958fc8155
yoonji <kyjkbw@gmail.com>
2025-02-25 12:34:31 +0900

    feat: 토큰 재발급 기능 추가 및 테스트

--- src/components/testLogin/TestLoginModal.tsx
+++ src/components/testLogin/TestLoginModal.tsx
@@ -1,4 +1,4 @@
-import { login, logout } from '@/apis/auth';
+import { login, logout, reissueToken } from '@/apis/auth';
 import { axiosInstance } from '@/apis/axios';
 import { useAuthStore } from '@/store/authStore';
 
@@ -27,23 +27,21 @@ const handleLogout = async () => {
 };
 
 const handleTokenReissue = async () => {
-  console.log('AT 재발급 시도');
   try {
-    const { data } = await axiosInstance.post('/auth/token');
-    console.log('AT 재발급:', data);
-
-    if (!data.data) {
-      console.log('토큰 정상');
-      return;
-    }
-
-    if (data.data.accessToken) {
-      console.log('토큰 재발급 성공:', data.data.accessToken);
-    }
+    await reissueToken();
   } catch (error) {
     console.error(error);
-    //window.location.href = '/'; // 로그인 페이지로 이동
-    throw error;
+  }
+};
+
+//test
+const test = async () => {
+  try {
+    console.log('테스트 시작');
+    const data = await axiosInstance.get('/user');
+    console.log('테스트:', data);
+  } catch (error) {
+    console.error(error);
   }
 };
 
@@ -65,6 +63,9 @@ export default function TestLoginModal() {
             className="caption-r bg-black/30 text-white px-1 py-1 rounded"
           >
             rt 재발급
+          </button>
+          <button onClick={test} className="caption-r bg-black/30 text-white px-1 py-1 rounded">
+            테스트
           </button>
         </>
       ) : (

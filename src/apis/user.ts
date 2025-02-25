7fdccfc4b2f34bc875dcd432ee2fc50ae1afd1c8
kangsuyeong <ekdh16@naver.com>
2025-02-26 07:37:54 +0900

    feat: 회원가입 api추가

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -7,3 +7,29 @@ export const getNicknameAvailability = async (nickname: string) => {
   });
   return response;
 };
+
+// 아이디 중복를 확인하는 함수
+export const getIdAvailability = async (loginId: string) => {
+  const response = await axiosInstance.get('user/checkLoginId', {
+    params: { loginId },
+  });
+  return response;
+};
+
+// 회원가입
+export const postSignUp = async (
+  nickName: string,
+  loginId: string,
+  password: string,
+  email: string,
+) => {
+  const response = await axiosInstance.post('/user/signup', {
+    nickName,
+    socialId: null,
+    socialType: 'NONE',
+    loginId,
+    password,
+    email,
+  });
+  return response;
+};

fb8ddcc7f20b4ec0fc4e8087d8a0439ce24273ac
kangsuyeong <ekdh16@naver.com>
2025-02-26 14:11:05 +0900

    feat: button 기본 타입추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,15 @@
 import { twMerge } from 'tailwind-merge';
 
-interface ButtonProps {
-  children: React.ReactNode;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary' | 'disabled';
-  className?: string;
-  onClick?: () => void;
 }
 
-export default function Button({ children, variant = 'primary', className, onClick }: ButtonProps) {
+export default function Button({
+  children,
+  variant = 'primary',
+  className,
+  ...props
+}: ButtonProps) {
   const buttonStyle = {
     primary: 'bg-primary-normal hover:bg-primary-hover',
     secondary: 'bg-white border border-primary-active text-primary-active hover:bg-gray-5',
@@ -23,7 +25,7 @@ export default function Button({ children, variant = 'primary', className, onCli
           className,
         )}
         disabled={variant === 'disabled'}
-        onClick={variant === 'disabled' ? undefined : onClick}
+        {...props}
       >
         {children}
       </button>

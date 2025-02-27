5c2c586d8bf20ef414cbbe972c89cd77d2e0c21b
kangsuyeong <ekdh16@naver.com>
2025-02-27 11:11:25 +0900

    feat: 카드 / 노래 모달시트 관리 추가

--- src/store/sheetStore.ts
+++ src/store/sheetStore.ts
@@ -1,13 +1,26 @@
 import { create } from 'zustand';
 
 interface SheetStore {
-  isSheetOpen: boolean;
-  openSheet: () => void;
-  closeSheet: () => void;
+  isMusicSheetOpen: boolean;
+  isCardSheetOpen: boolean;
+  openSheet: (sheetName: string) => void;
+  closeSheet: (sheetName: string) => void;
+  closeAllSheets: () => void;
 }
 
 export const useSheetStore = create<SheetStore>((set) => ({
-  isSheetOpen: false,
-  openSheet: () => set({ isSheetOpen: true }),
-  closeSheet: () => set({ isSheetOpen: false }),
+  isMusicSheetOpen: false,
+  isCardSheetOpen: false,
+  openSheet: (sheetName) => set((prevState) => ({ ...prevState, [sheetName]: true })), // 개별 오픈
+  closeSheet: (sheetName) => set((prevState) => ({ ...prevState, [sheetName]: false })), // 개별 닫기
+  closeAllSheets: () => set({ isMusicSheetOpen: false, isCardSheetOpen: false }), // 한 번에 닫기
 }));
+
+// 사용예시
+// 카드 상세 모달시트 띄우기
+// openSheet('isCardSheetOpen')
+// 노래 검색 모달시트 띄우기
+// openSheet('isMusicSheetOpen')
+
+// 둘다 창 지우기
+// closeAllSheets()

0ae69429130b152c1ae7384a419af7484cbfc489
kangsuyeong <ekdh16@naver.com>
2025-02-27 11:07:19 +0900

    feat: 유저별 감정 기록 조회 추가

--- src/apis/emotionRecord.ts
+++ src/apis/emotionRecord.ts
@@ -14,3 +14,11 @@ export const postEmotionRecord = async (emotionRecord: EmotionRecordRequest) => 
   const { data } = await axiosInstance.post(`/emotion`, emotionRecord);
   return data;
 };
+
+// 유저별 감정 기록 조회
+export const getUserEmotionRecords = async (tag: string, page = 0, size = 10) => {
+  const { data } = await axiosInstance.get('/emotion/user', {
+    params: { tag, page, size },
+  });
+  return data;
+};

7036f339a1ff2a23afbc42304f5d25bb4a156f69
yoonji <kyjkbw@gmail.com>
2025-03-11 12:19:37 +0900

    fix: useUserStatus 훅에서 userId를 loginId로 변경

--- src/hooks/useUserStatus.ts
+++ src/hooks/useUserStatus.ts
@@ -1,19 +1,17 @@
 import { useEffect, useState } from 'react';
 
-function useUserStatus(userId: string) {
-  const [isChatting, setIsChatting] = useState(false);
+function useUserStatus(loginId: string) {
+  const [isChatting, setIsChatting] = useState(true);
 
   useEffect(() => {
-    if (!userId) return;
-
     const eventSource = new EventSource(
-      `http://localhost:8080/api/userStatus/subscribe?userId=${userId}`,
+      `http://43.203.98.65:8080/api/userStatus/subscribe?loginId=${loginId}`,
     );
 
     eventSource.onmessage = (event) => {
       try {
         const data = JSON.parse(event.data); // 서버에서 오는 데이터가 JSON 형식이라면 파싱
-        console.log('SSE 데이터 수신:', data);
+        console.log('SSE 데이터수신:', data);
 
         // 여기서 받은 데이터에 따라 상태 업데이트
         setIsChatting(data.isChatting); // 예제에서는 `isChatting`을 받는다고 가정
@@ -30,7 +28,7 @@ function useUserStatus(userId: string) {
     return () => {
       eventSource.close(); // 컴포넌트가 언마운트되면 연결 종료
     };
-  }, [userId]);
+  }, [loginId]);
 
   return { isChatting };
 }

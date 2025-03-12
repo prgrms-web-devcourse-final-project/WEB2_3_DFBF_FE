e9e60abf662dd41712b1f1c1cfd63bd46b39df17
denshou <zecaumr@naver.com>
2025-03-12 15:56:44 +0900

    fix: 레이아웃 테스트

--- src/layouts/AnimatedLayout.tsx
+++ src/layouts/AnimatedLayout.tsx
@@ -40,7 +40,12 @@ function AnimatedLayout({ children }: { children: React.ReactNode }) {
         </motion.div>
       </AnimatePresence>
       {/* 양옆에 배경을 만드는 Grid 컨테이너 */}
-      <div className="absolute top-0 left-0 right-0 bottom-0 grid grid-cols-[1fr_minmax(0,600px)_1fr]">
+      <div
+        className={twMerge(
+          'absolute top-0 left-0 right-0 bottom-0 grid grid-cols-[1fr_minmax(0,600px)_1fr]',
+          'max-[600px]:hidden',
+        )}
+      >
         {/* 왼쪽 배경 */}
         <div className="w-full h-full bg-white z-50"></div>
         {/* 실제 내용이 들어가는 부분 */}

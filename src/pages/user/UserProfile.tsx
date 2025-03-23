70580ab38cbf76b0ccbc57a61a22ffe1722f79d1
kangsuyeong <ekdh16@naver.com>
2025-03-23 09:22:27 +0900

    feat: 로딩 컴포넌트 추가

--- src/pages/user/UserProfile.tsx
+++ src/pages/user/UserProfile.tsx
@@ -6,6 +6,8 @@ import { useNavigate } from 'react-router';
 import { useDeleteEmotionRecord } from '@/hooks/useDeleteEmotionRecord';
 import UserProfileInfo from '@/pages/user/components/UserProfileInfo/UserProfileInfo';
 import UserEmotionRecordList from '@/pages/user/components/UserEmotionRecordList/UserEmotionRecordList';
+import { useIsFetching } from '@tanstack/react-query';
+import Loading from '@/components/loading/Loading';
 
 interface UserProfileProps {
   isMyPage?: boolean; // 마이페이지 여부 확인 -> false면 유저페이지
@@ -13,6 +15,8 @@ interface UserProfileProps {
 
 // 마이페이지 / 유저페이지 UI
 function UserProfile({ isMyPage = false }: UserProfileProps) {
+  const isFetching = useIsFetching(); // 전체 쿼리 중 fetch 중인 게 있는지
+
   const navigate = useNavigate();
 
   const { openSheet, closeSheet } = useSheetStore(); // 시트
@@ -67,6 +71,7 @@ function UserProfile({ isMyPage = false }: UserProfileProps) {
           handleEdit={handleEdit}
         />
       )}
+      {isFetching && <Loading />}
     </>
   );
 }

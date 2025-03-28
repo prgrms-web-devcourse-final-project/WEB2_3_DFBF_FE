b1445061a7128268ea550c2183800dc9871d31e5
yoonji <kyjkbw@gmail.com>
2025-03-28 11:12:08 +0900

    refactor: 버튼, 성공실패 훅으로 변경

--- src/pages/post/Post.tsx
+++ src/pages/post/Post.tsx
@@ -1,56 +1,40 @@
-import Button from '@/components/Button';
 import { useEffect } from 'react';
 import { useParams } from 'react-router';
-import SpinLoading from '@/components/loading/SpinLoading';
-import Complete from '@/components/loading/Complete';
-import ErrorShake from '@/components/loading/ErrorShake';
-import { useQueryClient } from '@tanstack/react-query';
+import PostForm from '@/pages/post/components/PostForm';
+import { fetchSpotifyVideoId } from '@/utils/fetchSpotifyVideoId';
 import usePostForm from '@/hooks/post/usePostForm';
 import useMusicSelection from '@/hooks/post/useMusicSelection';
-import PostForm from '@/pages/post/components/PostForm';
 import usePostSubmit from '@/hooks/post/usePostSubmit';
-import { fetchSpotifyVideoId } from '@/utils/fetchSpotifyVideoId';
 import useEditInit from '@/hooks/post/useEditInit';
-import usePostModals from '@/hooks/post/usePostModals';
+import PostSubmitButton from '@/pages/post/components/PostSubmitButton';
+import usePostSubmitHandler from '@/hooks/post/usePostSubmitHandler';
 
 export default function Post() {
   const { postId } = useParams();
   const isEditMode = Boolean(postId); // 수정 모드인지 확인
   const mode = isEditMode ? 'edit' : 'create';
 
-  const queryClient = useQueryClient();
-
-  const { selectedEmotion, comment, onEmotionClick, onChangeComment, isFilled } = usePostForm();
+  const { formData, setEmotion, setComment, isComment } = usePostForm(); // 폼 관련 훅(감정, 코멘트)
   const { selectedPostMusic, isMusicSelect, clearPostMusic } = useMusicSelection(); // 음악 선택 상태
-  const { showSuccessModal, showFailModal } = usePostModals(isEditMode); // 모달 관련 훅
-  useEditInit(isEditMode ? Number(postId) : null); // 수정 모드일 때 초기값
-
-  // 기록 완료 조건 확인
-  const isCompletePost = selectedPostMusic && isFilled;
-
-  // 글 등록 성공
-  const handlePostSuccess = () => {
-    showSuccessModal();
-    queryClient.invalidateQueries({ queryKey: ['userPosts', 'me'] });
-  };
-
-  // 글 등록 실패
-  const handlePostError = (error: Error) => {
-    console.error(error);
-    showFailModal();
-  };
+  useEditInit({ postId: isEditMode ? Number(postId) : null, setEmotion, setComment }); // 수정 모드일 때 초기값
+  const { handlePostError, handlePostSuccess } = usePostSubmitHandler({
+    mode,
+  }); // 글 등록 성공, 실패 핸들러
 
   // 글 등록
   const { onSubmit, isPending, isSuccess, isError } = usePostSubmit({
-    mode,
+    mode, // create, edit
     postId,
     onSuccess: handlePostSuccess,
     onError: handlePostError,
   });
 
+  // 기록 완료 조건 확인
+  const isCompletePost = selectedPostMusic && isComment && formData.emotion !== null;
+
   // 기록 완료
   const onCompletePost = async () => {
-    if (!isCompletePost || !selectedEmotion) return;
+    if (!isCompletePost) return;
 
     // videoId 조회
     const videoId = await fetchSpotifyVideoId(
@@ -59,27 +43,17 @@ export default function Post() {
       selectedPostMusic?.title,
     );
 
-    const data = {
+    const data: EmotionRecordRequest = {
       spotifyId: selectedPostMusic?.spotifyId,
       videoId,
       title: selectedPostMusic?.title,
       artist: selectedPostMusic?.artist,
       albumImage: selectedPostMusic?.albumImage,
-      emotion: selectedEmotion,
-      comment,
+      emotion: formData.emotion as string,
+      comment: formData.comment,
     };
 
     onSubmit(data);
-  };
-
-  const renderButtonContent = () => {
-    if (isPending) {
-      return <SpinLoading />;
-    } else if (isSuccess) {
-      return <Complete />;
-    } else if (isError) {
-      return <ErrorShake />;
-    } else return isEditMode ? <span>수정 완료</span> : <span>기록 완료</span>;
   };
 
   useEffect(() => {
@@ -91,21 +65,22 @@ export default function Post() {
   return (
     <div className="flex flex-col items-center justify-between w-full pb-10">
       <PostForm
-        onEmotionClick={onEmotionClick}
-        selectedEmotion={selectedEmotion}
+        onEmotionClick={setEmotion}
+        selectedEmotion={formData.emotion}
         selectedPostMusic={selectedPostMusic}
         isMusicSelect={isMusicSelect}
-        comment={comment}
-        onChangeComment={onChangeComment}
+        comment={formData.comment}
+        onChangeComment={(e) => setComment(e.target.value)}
       />
       {/* 버튼 */}
-      <Button
-        variant={isCompletePost ? 'primary' : 'disabled'}
-        className={isError ? 'bg-functional-danger' : ''}
-        onClick={onCompletePost}
-      >
-        {renderButtonContent()}
-      </Button>
+      <PostSubmitButton
+        isEditMode={isEditMode}
+        isCompletePost={isCompletePost}
+        isPending={isPending}
+        isSuccess={isSuccess}
+        isError={isError}
+        onCompletePost={onCompletePost}
+      />
     </div>
   );
 }

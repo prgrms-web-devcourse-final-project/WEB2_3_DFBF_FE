f89644aee42f6368809f8e0e7c09e019c5b1a96e
yoonji <kyjkbw@gmail.com>
2025-03-28 11:10:39 +0900

    refactor: usePostModals 훅의 매개변수를 'create' | 'edit'로 변경

--- src/hooks/post/usePostModals.ts
+++ src/hooks/post/usePostModals.ts
@@ -1,14 +1,14 @@
 import { useModalStore } from '@/store/modalStore';
 import { useNavigate } from 'react-router';
 
-const usePostModals = (isEditMode: boolean) => {
+const usePostModals = (mode: 'create' | 'edit') => {
   const { openModal, closeModal } = useModalStore();
   const navigate = useNavigate();
 
   // 글 등록 성공 모달
   const showSuccessModal = () => {
     openModal({
-      title: isEditMode ? '글 수정 성공' : '글 등록 성공',
+      title: mode === 'edit' ? '글 수정 성공' : '글 등록 성공',
       message: '내가 쓴 글을 확인하러 가 볼까요?',
       confirmText: '확인하러 가기',
       cancelText: '홈으로 가기',
@@ -26,7 +26,7 @@ const usePostModals = (isEditMode: boolean) => {
   // 글 등록 실패 모달
   const showFailModal = () => {
     openModal({
-      title: isEditMode ? '글 수정 실패' : '글 등록 실패',
+      title: mode === 'edit' ? '글 수정 실패' : '글 등록 실패',
       message: '잠시 후 다시 시도해 주세요.',
       confirmText: '확인',
       onConfirm: async () => {

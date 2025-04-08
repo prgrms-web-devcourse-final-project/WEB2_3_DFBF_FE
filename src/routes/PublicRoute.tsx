ff69e829a6b7a3f586e05bd32903c2e3a4be7826
kangsuyeong <ekdh16@naver.com>
2025-04-08 17:06:04 +0900

    refactor: publicroute 적용

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -2,10 +2,9 @@ import { useAuthStore } from '@/store/authStore';
 import { Navigate, Outlet } from 'react-router';
 
 const PublicRoute = () => {
-
   const { isAuthenticated } = useAuthStore();
 
-  return isAuthenticated ? <Navigate to="/" replace /> : <Outlet />;
+  return isAuthenticated ? <Navigate to="/home" replace /> : <Outlet />;
 };
 
 export default PublicRoute;

e384774e0aac2d8a79076337b8a36c9ec54e88c0
kangsuyeong <ekdh16@naver.com>
2025-04-20 20:11:37 +0900

    design: layout 수정

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -20,38 +20,36 @@ function Layout() {
     location.pathname === '/chat';
 
   return (
-    <div className="flex justify-center w-full min-h-screen">
-      <div className="max-w-[600px] min-w-[320px] w-full bg-background flex flex-col">
-        {/* 헤더 */}
-        <HeaderWrapper />
+    <div className="max-w-[600px] min-w-[320px] w-full bg-background flex min-h-screen mx-auto">
+      {/* 헤더 */}
+      <HeaderWrapper />
 
-        {/* 메인 컨텐츠 영역 */}
-        <div
-          className={twMerge(
-            'pt-[44px] flex-1 flex justify-center w-full px-3',
-            showNav && 'pb-[62px] ', // 하단 네비게이션 숨길때만 padding주기
-          )}
-        >
-          <MyErrorBoundary>
-            <AnimatePresence mode="wait">
-              <motion.div
-                key={location.pathname} // 경로가 바뀔 때마다 애니메이션 트리거
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.3 }}
-                className="flex justify-center w-full h-full"
-              >
-                <Outlet />
-              </motion.div>
-            </AnimatePresence>
-          </MyErrorBoundary>
-        </div>
-
-        {/* 하단 네비게이션 */}
-        <BottomNavWrapper />
-        {/* 글작성 버튼 */}
-        <PostButton />
+      {/* 메인 컨텐츠 영역 */}
+      <div
+        className={twMerge(
+          'pt-[44px] flex-1 flex justify-center w-full px-3',
+          showNav && 'pb-[62px] ', // 하단 네비게이션 숨길때만 padding주기
+        )}
+      >
+        <MyErrorBoundary>
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={location.pathname} // 경로가 바뀔 때마다 애니메이션 트리거
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.3 }}
+              className="flex justify-center w-full h-full"
+            >
+              <Outlet />
+            </motion.div>
+          </AnimatePresence>
+        </MyErrorBoundary>
       </div>
+
+      {/* 하단 네비게이션 */}
+      <BottomNavWrapper />
+      {/* 글작성 버튼 */}
+      <PostButton />
     </div>
   );
 }

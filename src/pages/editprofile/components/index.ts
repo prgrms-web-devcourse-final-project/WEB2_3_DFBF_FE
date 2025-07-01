1e1f13b27166c608878c70b52a5512a4e9a0be88
kangsuyeong <ekdh16@naver.com>
2025-07-01 22:30:57 +0900

    feat: index.ts 정리

---
+++ src/pages/editprofile/components/index.ts
@@ -0,0 +1,4 @@
+export { default as CurrentPasswordInput } from './CurrentPasswordInput';
+export { default as PasswordEditForm } from './PasswordEditForm';
+export { default as ProfileEditForm } from './ProfileEditForm';
+export { default as TabMenu } from './TabMenu';

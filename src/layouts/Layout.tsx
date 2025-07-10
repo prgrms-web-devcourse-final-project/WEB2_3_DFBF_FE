eb7114d28c6a22cc941633291236c28edba0e650
kangsuyeong <ekdh16@naver.com>
2025-07-10 19:29:45 +0900

    refactor: Navbar 리펙토링

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,36 +1,19 @@
-import { Outlet, useLocation } from 'react-router';
+import { Outlet } from 'react-router';
 import { twMerge } from 'tailwind-merge';
 import PostButton from '@/components/PostButton';
 import MyErrorBoundary from '@/components/ErrorBoundary';
-import BottomNavWrapper from '@/layouts/bottomNav/BottomNavWrapper';
 import HeaderWrapper from '@/layouts/header/HeaderWrapper';
 import { AnimatePresence, motion } from 'framer-motion';
+import BottomNav from '@/layouts/bottomNav/BottomNav';
 
 function Layout() {
-  const location = useLocation(); // 현재 URL 가져오기
-  //바텀 nav 바 필요한 페이지
-  const showNav =
-    //메인 페이지
-    location.pathname === '/home' ||
-    //마이 페이지
-    location.pathname === '/mypage' ||
-    //유저 페이지
-    location.pathname.includes('/user') ||
-    //지난 대화 기록 페이지
-    location.pathname === '/chat';
-
   return (
     <div className="max-w-[600px] min-w-[320px] w-full bg-background flex min-h-screen mx-auto">
       {/* 헤더 */}
       <HeaderWrapper />
 
       {/* 메인 컨텐츠 영역 */}
-      <div
-        className={twMerge(
-          'pt-[44px] flex-1 flex justify-center w-full px-3',
-          showNav && 'pb-[62px] ', // 하단 네비게이션 숨길때만 padding주기
-        )}
-      >
+      <div className={twMerge('pt-[44px] flex-1 flex justify-center w-full px-3')}>
         <MyErrorBoundary>
           <AnimatePresence mode="wait">
             <motion.div
@@ -47,7 +30,7 @@ function Layout() {
       </div>
 
       {/* 하단 네비게이션 */}
-      <BottomNavWrapper />
+      <BottomNav />
       {/* 글작성 버튼 */}
       <PostButton />
     </div>

1ce0159001dbe6738481445ac901689f6c3d166a
kangsuyeong <ekdh16@naver.com>
2025-07-17 10:57:43 +0900

    chore: 필요없는 파일 삭제

--- src/pages/user/components/UserProfileInfo/UserProfileInfo.tsx
+++
@@ -1,8 +0,0 @@
-import MyProfileInfo from '@/pages/user/components/UserProfileInfo/MyProfileInfo';
-import OtherProfileInfo from '@/pages/user/components/UserProfileInfo/OtherProfileInfo';
-
-const UserProfileInfo = ({ isMyPage }: { isMyPage: boolean }) => {
-  return isMyPage ? <MyProfileInfo /> : <OtherProfileInfo />;
-};
-
-export default UserProfileInfo;

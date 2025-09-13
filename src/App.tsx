086ed2a693bac1aa7cc9ac7f5b823a67f385898b
kangsuyeong <ekdh16@naver.com>
2025-09-13 15:34:41 +0900

    refactor: pages 파일로 export 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
 import { Route, Routes } from 'react-router';
 import Layout from '@/layouts/Layout';
-import Landing from '@/pages/landing/Landing';
 import Modal from '@/components/Modal';
-import Home from '@/pages/home/Home';
 import ChatConnectLoadingSheet from '@/components/ChatConnectLoadingSheet/ChatConnectLoadingSheet';
-import Chat from '@/pages/chat/Chat';
-import ChatRoom from '@/pages/chat/ChatRoom';
-import NotFound from '@/pages/NotFound';
-import Login from '@/pages/Login';
-import SignUp from '@/pages/signup/SignUp';
-import Post from '@/pages/post/Post';
 import PrivateRoute from './routes/PrivateRoute';
-import EditProfile from '@/pages/editprofile/EditProfile';
-import BlockList from '@/pages/blocklist/BlockList';
 import YouTubeAudioPlayer from './components/YouTubeAudioPlayer';
-
 import { useSheetStore } from './store/sheetStore';
 import KaKaoRedirection from '@/components/KaKaoRedirection';
 import { useSSE } from '@/hooks/useSSE';
 import { useYotube } from '@/hooks/useYoutube';
 import PublicRoute from '@/routes/PublicRoute';
 import CardDetailModalTemp from '@/components/modalSheet/CardDetailModalTemp';
-import MyPage from '@/pages/mypage/MyPage';
-import UserPage from '@/pages/userpage/UserPage';
+import {
+  Home,
+  Landing,
+  Chat,
+  ChatRoom,
+  NotFound,
+  Login,
+  SignUp,
+  Post,
+  EditProfile,
+  BlockList,
+  MyPage,
+  UserPage,
+} from '@/pages';
 
 function App() {
   const { isRequestSendingSheetOpen, isRequestReceivingSheetOpen } = useSheetStore();
---
+++ src/pages/index.ts
@@ -0,0 +1,27 @@
+import Home from '@/pages/home/Home';
+import Landing from './landing/Landing';
+import Chat from '@/pages/chat/Chat';
+import ChatRoom from '@/pages/chat/ChatRoom';
+import NotFound from '@/pages/NotFound';
+import Login from '@/pages/Login';
+import SignUp from '@/pages/signup/SignUp';
+import Post from '@/pages/post/Post';
+import EditProfile from '@/pages/editprofile/EditProfile';
+import BlockList from '@/pages/blocklist/BlockList';
+import MyPage from '@/pages/mypage/MyPage';
+import UserPage from '@/pages/userpage/UserPage';
+
+export {
+  Landing,
+  Home,
+  Chat,
+  ChatRoom,
+  NotFound,
+  Login,
+  SignUp,
+  Post,
+  EditProfile,
+  BlockList,
+  MyPage,
+  UserPage,
+};

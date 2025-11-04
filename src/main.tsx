d7f951894ba434a65fb4d7088fad0effd2baf994
kangsuyeong <ekdh16@naver.com>
2025-11-04 10:39:31 +0900

    chore: 필요없는 코드 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { registerSW } from 'virtual:pwa-register';
+// import { registerSW } from 'virtual:pwa-register';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './styles/index.css';
 import App from './App.tsx';
@@ -9,16 +9,16 @@ import ScrollToTop from '@/components/ScrollToTop.tsx';
 import { queryClient } from '@/utils';
 
 // PWA 서비스 워커 등록
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm('새로운 버전이 있습니다. 새로고침할까요?')) {
-      updateSW(true);
-    }
-  },
-  onOfflineReady() {
-    // // console.log('PWA가 오프라인에서도 사용할 준비가 되었습니다!');
-  },
-});
+// const updateSW = registerSW({
+//   onNeedRefresh() {
+//     if (confirm('새로운 버전이 있습니다. 새로고침할까요?')) {
+//       updateSW(true);
+//     }
+//   },
+//   onOfflineReady() {
+//     // // console.log('PWA가 오프라인에서도 사용할 준비가 되었습니다!');
+//   },
+// });
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
